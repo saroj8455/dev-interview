2a11c5ae4311832cfec5b61848d856323f0278f8
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-09-10 23:09:44 +0530

    initial node express starter app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dev-interview
 Interview preparation for javascript and node js developer
+
+# packages for node app development
+
+express,helmet,xss-clean,mongoSanitize,
+cors,http-status,mongoose,morgan,winston,dotenv,
+path,joi,jsonwebtoken
--- nodeapp/src/index.js
+++ nodeapp/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+import express from "express"
+import {HELLO} from "../utils/message.js"
+
+const app = express();
+const PORT = 3000 || process.env.PORT
+
+app.get("/",(req,res)=>{
+    res.send("Hello World App");
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running http://localhost:${PORT}`)
+})
